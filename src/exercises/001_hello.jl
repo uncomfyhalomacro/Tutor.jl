5bbc40b433f759c4d674bc9d246c28adda7d658b
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-03-06 11:20:51 +0800

    Update Project.toml

---
+++ src/exercises/001_hello.jl
@@ -0,0 +1,14 @@
+# Oh no! This program needs your help!
+#
+# It is supposed to print "Hello world!".
+#
+# Julia declare strings with double quotes ("") and chars
+# as single quotes ('')
+# 
+# Try to fix the program by running `Tutor.run()` inside the
+# Julia REPL
+#
+
+function hello()
+  println("Hello world')
+end
