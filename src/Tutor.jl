1413c41b50297f9d93f969208a34ad4b59cd3417
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-03-10 13:41:44 +0800

    Improve logic

--- src/Tutor.jl
+++ src/Tutor.jl
@@ -3,20 +3,44 @@ module Tutor
 using Test
 using Dates
 
-function run()
+using REPL: REPL
+using REPL.TerminalMenus
+
+const EXERCISES = ["001", "002", "003", "004"]
+
+function choose_exercise(exercise)
+    menu = RadioMenu(exercise; pagesize=5)
+    choice = request("Choose an exercise:", menu)
+    if choice != -1
+        @info "You chose $(exercise[choice])"
+        return exercise[choice]
+    end
+    @warn "Menu canceled."
+    return nothing
+end
+
+function start()
     PROJECT_DIR = dirname(Base.current_project(@__DIR__))
 
+    chosen_exercise = choose_exercise(Tutor.EXERCISES)
     while true
         try
-            cmdlet = `julia --project="$PROJECT_DIR" "$PROJECT_DIR"/src/exercises/001_hello.jl`
+            cmdlet = `julia --project="$PROJECT_DIR" "$PROJECT_DIR"/src/exercises/"$chosen_exercise"_hello.jl`
             Base.run(cmdlet)
             @info "You have finally fixed the program! It is happy now thanks to you ❤️ "
-            break
         catch e
             @warn "This program needs help!"
             EDITOR = ENV["EDITOR"]
             cmdlet = `$EDITOR "$PROJECT_DIR/src/exercises/001_hello.jl"`
             Base.run(cmdlet)
+        end
+        println("Choose next exercise? y/n yes/no")
+        continue_run = readline()
+        if lowercase(continue_run) in ["y", "yes"]
+            chosen_exercise = choose_exercise(Tutor.EXERCISES)
+        else
+            @info "Ending Tutor"
+            break
         end
     end
 end
